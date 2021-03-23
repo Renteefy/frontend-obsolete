c1fcf7560981ccdf87f325e6aa868fa161bb6023
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2021-03-23 13:27:26 +0530

    login page layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,14 +11,44 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
+    // const colour = const Color(0xfEF476f);
     return MaterialApp(
-      home: Scaffold(backgroundColor: Colors.black,
-          appBar: AppBar(title: Text('Homepage')), 
-          // body: TodayView(),
-          body: Text("Hello")
+        home: Scaffold(
+      backgroundColor: Colors.black,
+      // body: TodayView(),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Positioned(
+              top: 100,
+              left: 60,
+              child: 
+                Text("Welcome to",
+                    style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold)))),
+          Positioned(
+            top: 150,
+            left: 60,
+            child: Text("Renteefy",
+                    style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                            color: Color(0xffEF476f),
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold)))
+            ),
+          Positioned(
+              child:
+                  Image(image: NetworkImage("https://i.imgur.com/gNf5rxI.png")),
+              width: 500,
+              right: -63)
+        ],
+      ),
+      // bottomSheet: Image(image: AssetImage("vector-creator.png")),
     ));
   }
 }
