02cb09778f2d9b5fd7f915997f69314de50d6cc7
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2021-03-23 22:16:38 +0530

    Homescreen done

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatefulWidget {
   }
 }
 
+String _email;
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
@@ -24,6 +26,7 @@ class _MyAppState extends State<MyApp> {
               margin: EdgeInsets.all(25),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SizedBox(
                     height: 50,
@@ -49,36 +52,69 @@ class _MyAppState extends State<MyApp> {
                               fontSize: 23, fontWeight: FontWeight.bold))),
                   SizedBox(height: 50),
                   TextField(
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    decoration: InputDecoration(
+                      fillColor: Color(0xff434040),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xff434040), width: 0),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      decoration: InputDecoration(
-                        fillColor: Color(0xff434040),
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              color: Color(0xff434040), width: 0),
-                          borderRadius: BorderRadius.circular(10.0),
+                      disabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xff434040), width: 0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xff434040), width: 0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color(0xff434040), width: 0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      hintText: "Email",
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _email = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xff3cdbd3), // background
+                          // onPrimary: Colors.white, // foreground
                         ),
-                        disabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              color: Color(0xff434040), width: 0),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              color: Color(0xff434040), width: 0),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              color: Color(0xff434040), width: 0),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        hintText: "Email",
-                        contentPadding: EdgeInsets.all(20),
-                      ))
+                        onPressed: () {
+                          print(_email);
+                          // call API here
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(22),
+                          child: Text("Let's Go",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        )),
+                  ),
+                  Container(
+                    child: Image.network(
+                      "https://i.imgur.com/gNf5rxI.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
                 ],
               ),
             )));
